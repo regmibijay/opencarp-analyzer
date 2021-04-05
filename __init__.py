ff80349b72e2e2e73a9992bb6ea5631e1ba78b7c
regmibijay <wilson_blackman@yahoo.com>
2021-04-05 10:06:43 +0200

    removed hard coded version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,2 @@
 from .opencarp_analyzer import *
 name = "opencarp-analyzer"
-__version__ = "1.0.0"
--- opencarp_analyzer/__init__.py
+++ opencarp_analyzer/__init__.py
@@ -1,4 +1,3 @@
 from .analyzer  import *
 from .plotter import *
 name = "opencarp-analyzer"
-__version__ = "1.0.0"
